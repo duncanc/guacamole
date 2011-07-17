5b8245025a4521dcb70f4a3a824b2e48ebf17839
Duncan Cross <duncan.cross@gmail.com>
2011-07-17 12:35:51 +0100

    Add simple console-based test launch system

---
+++ launchtest.lua
@@ -0,0 +1,27 @@
+
+local lfs = require 'lfs'
+
+local tests = {}
+for test in lfs.dir('tests') do
+	test = 'tests/' .. test
+	if (lfs.attributes(test, 'mode') == 'file') and test:lower():match('%.lua$') then
+		tests[#tests+1] = test
+	end
+end
+
+local test
+while true do
+	print('Please select a test:')
+	for i, test in ipairs(tests) do
+		print(string.format('[%d] %s', i, test))
+	end
+	io.write('>')
+	local selection = tonumber(io.read('*l'))
+	selection = selection and tests[selection]
+	if selection then
+		test = selection
+		break
+	end
+end
+
+dofile(test)
